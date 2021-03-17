d8ea9f2e17092cb0d66a979a1da6d476186cb458
Ido Nov <idonov8@gmail.com>
2021-03-17 16:56:40 +0200

    configure for firebase hosting

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include_once("home.html");
-include_once("vendor/bootstrap/css/bootstrap.min.css");
-include_once("assets/Nov.jpg");
-include_once("assets/Bensha.jpg");
-include_once("vendor/jquery/jquery.min.js");
-include_once("vendor/bootstrap/js/bootstrap.bundle.min.js");
-
-?>
