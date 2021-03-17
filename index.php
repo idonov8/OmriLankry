e0a7a7cdfe68dea33d898029bdb1a2e4cd2d91a3
Ido Nov <idonov8@gmail.com>
2021-03-17 03:21:58 +0200

    trying something

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php header( 'Location: /home.html' ) ;  ?>
+<?php 
+header( 'Location: /home.html' ) ;  
+include_once("vendor/bootstrap/css/bootstrap.min.css");
+include_once("assets/Nov.jpg");
+include_once("assets/Bensha.jpg");
+include_once("vendor/jquery/jquery.min.js");
+include_once("vendor/bootstrap/js/bootstrap.bundle.min.js");
+
+?>
