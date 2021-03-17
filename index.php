f19d946ed1527a07d7e18d0857ab1463ea7b7549
Ido Nov <idonov8@gmail.com>
2021-03-17 03:28:04 +0200

    hmm

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-header( 'Location: /home.html' ) ;  
+include_once("home.html");
 include_once("vendor/bootstrap/css/bootstrap.min.css");
 include_once("assets/Nov.jpg");
 include_once("assets/Bensha.jpg");
