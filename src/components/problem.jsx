2dad109b07a4139372de4548f549d71e64adc798
omer1BS <omer1.bs@gmail.com>
2021-05-01 22:54:34 +0300

    finished problem

--- src/components/problem.jsx
+++ src/components/problem.jsx
@@ -3,12 +3,12 @@ export const Problem = (props) => {
     <div id='features' className='text-center'>
       <div className='container'>
         <div className='col-md-10 col-md-offset-1 section-title'>
-          <h2>Features</h2>
+          <h2>Problem</h2>
         </div>
         <div className='row'>
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.title}-${i}`} className='col-xs-6 col-md-3'>
+                <div key={`${d.title}-${i}`} className='col-xs-6 col-md-4'>
                   {' '}
                   <i className={d.icon}></i>
                   <h3>{d.title}</h3>
