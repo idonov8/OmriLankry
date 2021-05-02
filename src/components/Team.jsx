182f4fdca400e1ff608ab5f04bf63fd10e2277ef
Ido Nov <idonov8@gmail.com>
2021-05-02 14:35:52 +0300

    change team description & change Nov's picture

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -5,8 +5,7 @@ export const Team = (props) => {
         <div className='col-md-8 col-md-offset-2 section-title'>
           <h2>Meet the Team</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+          We are a team of 4 members, with various levels of experience. Some of us are students, soldiers, and working in a tech company. We have participated and won hackathons, and graduated excellence programs.
           </p>
         </div>
         <div id='row'>
